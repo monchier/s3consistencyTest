deee4250a3e081a5c1d51f752ee2772a3a91990d
Matteo Monchiero <matteo@trifacta.com>
2018-11-04 14:06:27 -0800

    update

--- go.mod
+++ go.mod
@@ -2,6 +2,5 @@ module s3test
 
 require (
 	github.com/aws/aws-sdk-go v1.15.68
-	github.com/caio/go-tdigest v2.1.0+incompatible
-	github.com/yourbasic/fenwick v0.0.0-20180312213232-5f8823d88d15 // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
 )
--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"bytes"
 	"flag"
 	"time"
-	"github.com/caio/go-tdigest"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 )
 
@@ -22,9 +21,9 @@ var (
 	namespace = "MatteoS3Test"
 )
 
-func timeTrack(start time.Time, name string, digest *tdigest.TDigest) {
+func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
-	digest.Add(float64(elapsed.Nanoseconds()))
+	log.Printf("Time elapsed: %2.2f", float64(elapsed) * 1e-6)
 }
 
 func putMetric(sess *session.Session, metricName string, value float64, enable bool) error {
@@ -46,49 +45,78 @@ func putMetric(sess *session.Session, metricName string, value float64, enable b
 	return nil
 }
 
-func updateAndTest(sess *session.Session, counter int, digest *tdigest.TDigest, delay time.Duration, cw bool, key string) error {
-	defer timeTrack(time.Now(), "updateAndtest", digest)
-	svc := s3.New(sess, &aws.Config{
-		Region: aws.String("us-west-2"),
-	})
-	reader := strings.NewReader(strconv.Itoa(counter))
+func doPut(svc *s3.S3, key string, value int64) error {
+	reader := strings.NewReader(strconv.FormatInt(value, 10))
 	_, err := svc.PutObject(new(s3.PutObjectInput).SetBucket(myBucket).SetKey(key).SetBody(reader))
 	if err != nil {
 		return fmt.Errorf("PUT failed", err)
 	}
+	return nil
+}
 
-	time.Sleep(delay)
-
+func doGet(svc *s3.S3, key string) (int64, error) {
 	getObjectResponse, err := svc.GetObject(new(s3.GetObjectInput).SetBucket(myBucket).SetKey(key))
 	if err != nil {
-		return fmt.Errorf("GET failed", err)
+		return 0, fmt.Errorf("GET failed", err)
 	}
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(getObjectResponse.Body)
-	number, err := strconv.Atoi(buf.String())
+	number, err := strconv.ParseInt(buf.String(), 10, 64)
 	if err != nil {
-		return fmt.Errorf("Conversion error", err)
+		return 0, fmt.Errorf("Conversion error", err)
 	}
-	if number != counter {
-		log.Printf("consistency violation number=%d != counter=%d ", number, counter)
+	return number, nil
+}
+
+func updateAndTest(sess *session.Session, counter int64, delay time.Duration, cw bool, key string) error {
+	svc := s3.New(sess, &aws.Config{
+		Region: aws.String("us-west-2"),
+	})
+
+	if err := doPut(svc, key, counter); err != nil {
+		return fmt.Errorf("Put error", err)
+	}
+
+	time.Sleep(delay)
+
+	number, err := doGet(svc, key);
+
+	if err != nil {
+		return fmt.Errorf("Get error", err)
+	}
+
+	nViolations := 0
+
+	for i := 0 ; number != counter && i < 1000; i++ {
+		nViolations++;
+		time.Sleep(100 * time.Millisecond)
+		number, err = doGet(svc, key);
+
+		if err != nil {
+			return fmt.Errorf("Get error", err)
+		}
+	}
+
+	if nViolations > 0 {
+		log.Printf("consistency violation number=%d != counter=%d, nViolations=%d\n", number, counter, nViolations)
 		if err := putMetric(sess, "consistencyViolation", 1.0, cw); err != nil {
 			return fmt.Errorf("Error in uploading metric", err)
 		}
 	}
+
 	return nil
 }
 
 func main() {
-	t, _ := tdigest.New()
-	iterations := flag.Int("iterations", 1, "number of iterations")
+	iterations := flag.Int64("iterations", 1, "number of iterations")
 	delay := flag.Int64("delay", 0, "delay in milliseconds")
 	cwEnabled := flag.Bool("cw", true, "enable cloudwatch")
 	key := flag.String("key", "testKey", "key")
 	flag.Parse()
-	fmt.Printf("iterations=%d\n", *iterations)
-	fmt.Printf("delay=%d\n", *delay)
-	fmt.Printf("cwEnabled=%t\n", *cwEnabled)
-	fmt.Printf("key=%s\n", *key)
+	log.Printf("iterations=%d\n", *iterations)
+	log.Printf("delay=%d\n", *delay)
+	log.Printf("cwEnabled=%t\n", *cwEnabled)
+	log.Printf("key=%s\n", *key)
 
 	conf := aws.Config{Region: aws.String("us-west-2")}
 	sess, err:= session.NewSession(&conf)
@@ -100,18 +128,12 @@ func main() {
 		log.Fatal(fmt.Errorf("Error in uploading metric", err))
 	}
 
-	for i := 0; *iterations == -1 || i < *iterations; i++ {
-		err := updateAndTest(sess, i, t, time.Duration(*delay * 1e6), *cwEnabled, *key)
+	for i := int64(0); *iterations == -1 || i < *iterations; i++ {
+		err := updateAndTest(sess, i, time.Duration(*delay * 1e6), *cwEnabled, *key)
 		if err != nil {
 			log.Fatal("done with error", err)
 		}
 	}
-
-	fmt.Printf("Count: %d\n", t.Count())
-	fmt.Printf("Median: %2.2f ms\n", t.Quantile(0.5) * 1e-6)
-	fmt.Printf("Q1: %2.2f ms\n", t.Quantile(0.25) * 1e-6)
-	fmt.Printf("Q3: %2.2f ms\n", t.Quantile(0.75) * 1e-6)
-	fmt.Printf("Mean: %2.2f ms\n", t.TrimmedMean(0, 1) * 1e-6)
 
 	if err := putMetric(sess, "end", 1.0, *cwEnabled); err != nil {
 		log.Fatal(fmt.Errorf("Error in uploading metric", err))
